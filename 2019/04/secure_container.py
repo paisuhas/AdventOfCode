6f9e389e3ada424c15ff860d9df5fefae0793826
Suhas Pai <spai@cs.wisc.edu>
2019-12-04 00:47:53 -0600

    Remove redundant str conversion

--- 2019/04/secure_container.py
+++ 2019/04/secure_container.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 from collections import Counter
 
-def can_be_password_part1(n):
-    num = str(n)
+def can_be_password_part1(num):
     return all(int(i) <= int(j) for i, j in zip(num, num[1:])) and any(i == j for i, j in zip(num, num[1:]))
 
 part1 = sum(map(can_be_password_part1, map(str, range(108457, 562042))))
