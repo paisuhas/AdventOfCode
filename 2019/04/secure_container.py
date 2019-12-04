336ff5dbf822f77538071eab62a756bf46c9e4b1
Suhas Pai <spai@cs.wisc.edu>
2019-12-04 00:43:20 -0600

    Further reduce computation

--- 2019/04/secure_container.py
+++ 2019/04/secure_container.py
@@ -5,14 +5,13 @@ def can_be_password_part1(n):
     num = str(n)
     return all(int(i) <= int(j) for i, j in zip(num, num[1:])) and any(i == j for i, j in zip(num, num[1:]))
 
-part1 = sum(map(can_be_password_part1, range(108457, 562042)))
-
+part1 = sum(map(can_be_password_part1, map(str, range(108457, 562042))))
 print(part1)
 assert(part1 == 2779)
 
-def can_be_password_part2(n):
-    return can_be_password_part1(n) and 2 in Counter(str(n)).values()
+def can_be_password_part2(num):
+    return all(int(i) <= int(j) for i, j in zip(num, num[1:])) and 2 in Counter(num).values()
 
-part2 = sum(map(can_be_password_part2, range(108457, 562042)))
+part2 = sum(map(can_be_password_part2, map(str, range(108457, 562042))))
 print(part2)
 assert(part2 == 1972)
