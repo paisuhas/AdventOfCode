f70cc668ed027bebf5b4160b7c2356d9cbfec4c2
Suhas Pai <spai@cs.wisc.edu>
2019-12-05 00:52:46 -0600

    Solution for AoC 2019 Day 05 Part 2

--- 2019/05/sunny_with_a_chance_of_asteroids.py
+++ 2019/05/sunny_with_a_chance_of_asteroids.py
@@ -13,6 +13,8 @@ next_pc = 0
 
 three_op_instructions = [1, 2]
 one_op_instructions = [3, 4]
+jump_instructions = [5, 6]
+comparison_instructions = [7, 8]
 
 for pc, opcode in enumerate(program):
     if pc == next_pc:
@@ -30,15 +32,27 @@ for pc, opcode in enumerate(program):
             program[program[pc + 3]] = result
         elif op in one_op_instructions:
             next_pc = pc + 2
-
             if op == 3:
-                program[program[pc+1]] = 1
+                program[program[pc+1]] = 5 # 1 for Part 1
             else:
                 assert(op == 4)
                 if modes[0]:
                     print(program[pc+1])
                 else:
                     print(program[program[pc+1]])
+        elif op in jump_instructions:
+            operands = [program[pc+offset] if mode else program[program[pc+offset]] for offset, mode in enumerate(modes[:-1], 1)]
+            if (op == 5 and operands[0]) or (op == 6 and not operands[0]):
+                next_pc = operands[1]
+            else:
+                next_pc = pc + 3
+        elif op in comparison_instructions:
+            next_pc = pc + 4
+            operands = [program[pc+offset] if mode else program[program[pc+offset]] for offset, mode in enumerate(modes[:-1], 1)]
+            if (op == 7 and operands[0] < operands[1]) or (op == 8 and operands[0] == operands[1]):
+                program[program[pc+3]] = 1
+            else:
+                program[program[pc+3]] = 0
         else:
             assert(op == 99)
             break
